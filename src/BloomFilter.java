064daeac18cf893e29a576fb7ee59482e06256d2
Lucas Sousa <botto@aluno-1755.wireless.ua.pt>
2019-11-25 12:03:23 +0000

    mudanças no bloom filter

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,13 +1,21 @@
 public class BloomFilter {
 
 	private int[] bfArray; // Declarar array que vai conter os valores do filtro
+	private int sizebfArray; // tamanho do bloom filter
 	private int fc; // Fator de Carga
-	private int n; // number of hash functions
+	private int hashfnct; // funçoes hash
+	private int nElementos; // numero de elementos
 
-	public BloomFilter(int fc, int n, int size) {
+	public BloomFilter(int fc, int nElementos) {
 		this.fc = fc;
-		this.n = n;
-		bfArray = new int[size];
+		this.sizebfArray = (int) Math.round((double) nElementos / fc);
+		this.nhash = (int) Math.round((sizebfArray / nElementos) * Math.log(2));
+
+	}
+
+	public int[] initbfArray() {
+		bfArray = new int[this.sizebfArray];
+		return bfArray;
 	}
 
 	public int[] getBfArray() {
@@ -33,7 +41,5 @@ public class BloomFilter {
 	public void setN(int n) {
 		this.n = n;
 	}
-	
-	
 
 }
